e4dc97efa39fc2c5cf5001c507e48a1640429643
Neeraj9080 <neerajpatel02468@gmail.com>
2025-03-07 11:32:01 +0530

    [Neeraj] Add. Updating component.ts file

--- HelloWorldApp/src/app/app.component.ts
+++ HelloWorldApp/src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'HelloWorldApp';
+  title = 'HelloWorld';
 
   ngOnInit(): void {
     this.title = "Hello from BridgeLabz";
