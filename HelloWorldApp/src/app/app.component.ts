fdab24c8bb5551dca188cb743ad756ca4295a4c3
Neeraj9080 <neerajpatel02468@gmail.com>
2025-03-08 12:25:23 +0530

    [Neeraj] Add. Implementing two-way binding for userName in BridgeLabz app to display personalized 'Hello' message

--- HelloWorldApp/src/app/app.component.ts
+++ HelloWorldApp/src/app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
+  imports: [RouterModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
@@ -9,12 +12,13 @@ export class AppComponent {
   title = 'HelloWorld';
   imgUrl = "assets/logo.jpg";
   url = "https://www.bridgelabz.com";
+  userName: string = '';
 
   ngOnInit(): void {
     this.title = "Hello from BridgeLabz";
   }
 
-  onClick($event: MouseEvent) {
+  onClick($event: any) {
     console.log("Save button is clicked!", $event);
     window.open(this.url, "_blank");
   }
